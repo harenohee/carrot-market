f20220004ab7a491644698da471fd40e99713d0e
harenohee <panda4859@gmail.com>
2023-02-01 21:02:46 +0900

    feat: 4.8 Tailwind CSS
    open selector 적용

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,11 +1,9 @@
 export default function Form() {
 	return (
 		<div className="flex flex-col space-y-2 p-5">
-			<details>
-				<summary className="select-none cursor-pointer">
-					What is my fav. food?
-				</summary>
-				<span className="selection:bg-indigo-500">김추ㅣ</span>
+			<details className="select-none open:text-white open:bg-indigo-500">
+				<summary className="cursor-pointer">What is my fav. food?</summary>
+				<span>김추ㅣ</span>
 			</details>
 		</div>
 	)
