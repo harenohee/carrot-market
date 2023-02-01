0a8cbe59473c90a8dc79f27f11654d18ff3d257c
harenohee <panda4859@gmail.com>
2023-02-01 18:43:05 +0900

    feat: 4.4 Tailwind CSS modifier 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,13 @@ export default function Home() {
           <span>Total</span>
           <span className="font-semibold">$38</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
-          Checkout
+        <div className="flex justify-center items-center">
+          <button
+            className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto
+          hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:text-gray-500"
+          >
+            Checkout
+          </button>
         </div>
       </div>
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
@@ -54,10 +59,10 @@ export default function Home() {
           <span className="font-medium text-xl">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
-            <div>
-              <input type="radio"></input>
-              <input type="radio"></input>
-              <input type="radio"></input>
+            <div className=" space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500"></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500"></button>
             </div>
             <div className="flex items-center space-x-5">
               <button className=" rounded-lg bg-blue-200 flex justify-center items-center  aspect-square w-8 font-medium  text-xl text-gray-500">
