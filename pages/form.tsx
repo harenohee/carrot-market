52d5060dd7f9932a0afa65ca44fb1768f4fa7708
harenohee <panda4859@gmail.com>
2023-02-01 21:05:28 +0900

    feat: 4.8 Tailwind CSS
    list-decimal - marker

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,10 +1,11 @@
 export default function Form() {
 	return (
 		<div className="flex flex-col space-y-2 p-5">
-			<details className="select-none open:text-white open:bg-indigo-500">
-				<summary className="cursor-pointer">What is my fav. food?</summary>
-				<span>김추ㅣ</span>
-			</details>
+			<ul className="list-decimal marker:text-teal-500">
+				<li>hi</li>
+				<li>hi</li>
+				<li>hi</li>
+			</ul>
 		</div>
 	)
 }
