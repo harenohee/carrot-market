5007d0f3217a66b0895f87f66faa9a7223d8f55b
harenohee <panda4859@gmail.com>
2023-02-01 22:29:14 +0900

    feat: 4.8 Tailwind CSS
    file-selector-button

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,11 +1,10 @@
 export default function Form() {
 	return (
 		<div className="flex flex-col space-y-2 p-5">
-			<ul className="list-decimal marker:text-teal-500">
-				<li>hi</li>
-				<li>hi</li>
-				<li>hi</li>
-			</ul>
+			<input
+				type="file"
+				className="file:cursor-pointer file:border-0 file:rounded-md file:bg-purple-400"
+			/>
 		</div>
 	)
 }
