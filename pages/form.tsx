4e8a3463a302ff6f68db6530b469983a237c1bb1
harenohee <panda4859@gmail.com>
2023-02-01 22:34:09 +0900

    feat: 4.8 Tailwind CSS
    first-letter selector

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,10 +1,13 @@
 export default function Form() {
 	return (
 		<div className="flex flex-col space-y-2 p-5">
-			<input
-				type="file"
-				className="file:cursor-pointer file:border-0 file:rounded-md file:bg-purple-400"
-			/>
+			{/* Lorem 입력 - 텍스트 자동 생성 */}
+			<p className="first-letter:text-2xl first-line:hover:text-purple-400">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+				praesentium, exercitationem dolorum eum ut magnam suscipit repudiandae
+				vitae minima corporis hic, architecto consequuntur obcaecati veritatis
+				deleniti alias? Quisquam, dignissimos assumenda.
+			</p>
 		</div>
 	)
 }
