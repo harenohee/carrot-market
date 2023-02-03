fadb62f060ac7eece2e483b0fa9e88ad20af731a
harenohee <panda4859@gmail.com>
2023-02-03 16:45:21 +0900

    feat: 소셜 로그인 버튼 ui
    absolute, relative
    
    #1 by @harenohee

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -13,7 +13,7 @@ export default function Enter() {
 	return (
 		<div className="mt-16 px-4">
 			<h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center mt-6">
 				<h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
 				<div className="grid border-b w-full pb-4  mt-8 grid-cols-2 gap-16">
 					<button
@@ -56,7 +56,7 @@ export default function Enter() {
 					{method === 'phone' ? (
 						// +82도 인풋에 속한 것처럼 느껴지도록 하기
 						<div className="flex rounded-md shadow-sm">
-							<span className="flex items-center justify-center px-3 rounded-l-md border-r-0 border border-gray-300 bg-gray-50 text-gray-500 selection:none">
+							<span className="flex items-center justify-center px-3 rounded-l-md border-r-0 border border-gray-300 bg-gray-50 text-sm text-gray-500 selection:none">
 								+82
 							</span>
 							<input
@@ -72,15 +72,19 @@ export default function Enter() {
 					{method === 'phone' ? 'Get one-time password' : null}
 				</button>
 			</form>
-			<div>
-				<div>
-					<div />
-					<div>
-						<span>Or enter with</span>
+			<div className="mt-6">
+				<div className="relative">
+					{/* 라인 */}
+					<div className="absoloute w-full border-t border-[gray]" />
+					{/* 라인 중앙에 겹치도록 */}
+					<div className="relative -top-3 text-center">
+						<span className="bg-white px-2 text-sm text-[gray]">
+							Or enter with
+						</span>
 					</div>
 				</div>
-				<div>
-					<button>
+				<div className="grid grid-cols-2 gap-3 mt-6">
+					<button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white font-medium text-sm text-gray-500 hover:bg-gray-50">
 						<svg
 							className="w-5 h-5"
 							aria-hidden="true"
@@ -90,7 +94,7 @@ export default function Enter() {
 							<path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
 						</svg>
 					</button>
-					<button>
+					<button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white font-medium text-sm text-gray-500 hover:bg-gray-50">
 						<svg
 							className="w-5 h-5"
 							aria-hidden="true"
