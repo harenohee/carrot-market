e7a76c9cc9beb03ff0b2788809a2e760f36710c3
harenohee <panda4859@gmail.com>
2023-02-06 20:54:33 +0900

    feat: 5.9 Chat Detail
    input창 안으로 버튼 넣기
    
    #6 by @harenohee

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const ChatDetail: NextPage = () => {
 	return (
-		<div className="py-10 px-4 space-y-4">
+		<div className="py-10 pb-16 space-y-4">
 			<div className="flex items-start space-x-2">
 				<div className="w-8 h-8 rounded-full bg-slate-500" />
 				<div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
@@ -22,11 +22,16 @@ const ChatDetail: NextPage = () => {
 					<p>미쳤어</p>
 				</div>
 			</div>
-			<div className="fixed w-full mx-auto max-w-md bottom-0 inset-x-0">
-				<div>
-					<input type="text" />
-					<div>
-						<span>&rarr;</span>
+			<div className="fixed py-2 bg-white  bottom-0 inset-x-0">
+				<div className="flex relative max-w-md items-center  w-full mx-auto">
+					<input
+						type="text"
+						className=" shadow-sm rounded-full w-full pr-12 border-gray-300 focus:ring-orange-500 focus:outline-none focus:border-orange-500"
+					/>
+					<div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+						<button className="flex focus:ring-2 ring-offset-2 ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
+							&rarr;
+						</button>
 					</div>
 				</div>
 			</div>
