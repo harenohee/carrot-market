0f86005654cf8c8a79b6cccc8ffe6ba77c00c5f2
harenohee <panda4859@gmail.com>
2023-02-06 13:35:52 +0900

    refactor: last modifier
    tailwind의 divider 속성을 이용하여 자동으로 border 적용
    
    #6 by @harenohee

--- pages/chats.tsx
+++ pages/chats.tsx
@@ -3,13 +3,13 @@ import type { NextPage } from 'next'
 const Chats: NextPage = () => {
 	return (
 		// border-b 선이 마지막 요소까지 생긴다.
-		// 해결방법: 1. last modifier
-		<div className="py-10">
+		// 해결방법: 1. last modifier 2. divider (tailwind css)
+		// divide: 어떤 요소 옆에 형제 요소가 있으면 border를 넣어줘~
+		<div className="py-10 divide-y-[1px]">
 			{[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-				// last:border-b-0
 				<div
 					key={i}
-					className="flex px-4 last:border-b-0  cursor-pointer py-3 border-b items-center space-x-3"
+					className="flex px-4 cursor-pointer py-3  items-center space-x-3"
 				>
 					<div className="w-12 h-12 rounded-full bg-slate-300" />
 					<div>
