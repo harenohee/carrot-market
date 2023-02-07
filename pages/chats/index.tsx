966f3a99386da2e6f1748276d9ea8c8a74953027
harenohee <panda4859@gmail.com>
2023-02-07 09:17:25 +0900

    feat: layout 컴포넌트 추가
    페이지에 layout 추가
    
    #9 by @harenohee

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,26 +1,29 @@
+import Layout from '@/components/layout'
 import type { NextPage } from 'next'
 
 const Chats: NextPage = () => {
 	return (
-		// border-b 선이 마지막 요소까지 생긴다.
-		// 해결방법: 1. last modifier 2. divider (tailwind css)
-		// divide: 어떤 요소 옆에 형제 요소가 있으면 border를 넣어줘~
-		<div className="py-10 divide-y-[1px]">
-			{[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-				<div
-					key={i}
-					className="flex px-4 cursor-pointer py-3  items-center space-x-3"
-				>
-					<div className="w-12 h-12 rounded-full bg-slate-300" />
-					<div>
-						<p className="text-gray-700">Steve Jebs</p>
-						<p className="text-sm  text-gray-500">
-							See you tomorrow in the corner at 2pm!
-						</p>
+		<Layout hasTabBar title="채팅">
+			{/*  border-b 선이 마지막 요소까지 생긴다. // 해결방법: 1. last modifier 2.
+			divider (tailwind css) // divide: 어떤 요소 옆에 형제 요소가 있으면
+			border를 넣어줘~ */}
+			<div className="py-10 divide-y-[1px]">
+				{[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+					<div
+						key={i}
+						className="flex px-4 cursor-pointer py-3  items-center space-x-3"
+					>
+						<div className="w-12 h-12 rounded-full bg-slate-300" />
+						<div>
+							<p className="text-gray-700">Steve Jebs</p>
+							<p className="text-sm  text-gray-500">
+								See you tomorrow in the corner at 2pm!
+							</p>
+						</div>
 					</div>
-				</div>
-			))}
-		</div>
+				))}
+			</div>
+		</Layout>
 	)
 }
 
