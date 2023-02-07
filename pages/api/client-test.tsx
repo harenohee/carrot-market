459761004e127c043469b1b79f2a68e48fda6ee3
harenohee <panda4859@gmail.com>
2023-02-07 23:03:48 +0900

    docs: api router 관련 주석 추가 -

--- pages/api/client-test.tsx
+++ pages/api/client-test.tsx
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import client from '../../libs/client'
 
+// create -> Promise를 반환 -> async await
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
@@ -15,4 +16,6 @@ export default async function handler(
 		ok: true,
 	})
 }
+// pages 경로에 api 폴더를 생성함으로써 api서버가 생성됨
+// api 라우트에서 클라이언트로 정보를 보내줄 수 있다.
 // 호스팅 중인 api 라우트를 통해 데이터베이스 수정이 가능
