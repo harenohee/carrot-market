464abd7f7f1837479756cff4106f5c2b350ec5bb
harenohee <panda4859@gmail.com>
2023-02-07 21:20:03 +0900

    refactor: 버튼, 인풋 컴포넌트화 컴포넌트에 타입스크립트 적용, 재사용성 증가  #11 by @harenohee

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,26 +1,23 @@
-import Layout from '@/components/layout'
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import Layout from '../../components/layout'
 
 const Chats: NextPage = () => {
 	return (
 		<Layout hasTabBar title="채팅">
-			{/*  border-b 선이 마지막 요소까지 생긴다. // 해결방법: 1. last modifier 2.
-			divider (tailwind css) // divide: 어떤 요소 옆에 형제 요소가 있으면
-			border를 넣어줘~ */}
-			<div className="py-10 divide-y-[1px]">
+			<div className="divide-y-[1px] ">
 				{[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-					<div
-						key={i}
-						className="flex px-4 cursor-pointer py-3  items-center space-x-3"
-					>
-						<div className="w-12 h-12 rounded-full bg-slate-300" />
-						<div>
-							<p className="text-gray-700">Steve Jebs</p>
-							<p className="text-sm  text-gray-500">
-								See you tomorrow in the corner at 2pm!
-							</p>
-						</div>
-					</div>
+					<Link legacyBehavior href={`/chats/${i}`} key={i}>
+						<a className="flex px-4 cursor-pointer py-3 items-center space-x-3">
+							<div className="w-12 h-12 rounded-full bg-slate-300" />
+							<div>
+								<p className="text-gray-700">Steve Jebs</p>
+								<p className="text-sm  text-gray-500">
+									See you tomorrow in the corner at 2pm!
+								</p>
+							</div>
+						</a>
+					</Link>
 				))}
 			</div>
 		</Layout>
