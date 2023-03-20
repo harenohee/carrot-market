01cec0a8453a091c6cceeb3ed36e62a7c6565dbb
harenohee <panda4859@gmail.com>
2023-03-20 12:21:28 +0900

    refactor: UseFormRegisterReturn 적용
    - [key: string]: any 대신 register의 type 명시
    
    #12 by @harenohee

--- components/input.tsx
+++ components/input.tsx
@@ -1,16 +1,24 @@
+import React from 'react'
+import type { UseFormRegisterReturn } from 'react-hook-form'
 interface InputProps {
 	label: string
 	name: string
 	kind?: 'text' | 'phone' | 'price'
-	[key: string]: any // 모든 prop을 받을 수 있게 처리
+	type: string
+	register: UseFormRegisterReturn
+	required: boolean
+	// [key: string]: any // 모든 prop을 받을 수 있게 처리
 }
 
 export default function Input({
 	label,
 	name,
 	kind = 'text',
-	...rest // 이렇게
-}: InputProps) {
+	register,
+	type,
+	required,
+}: // ...rest // 마지막 요소여야함
+InputProps) {
 	return (
 		<div>
 			<label
@@ -23,7 +31,10 @@ export default function Input({
 				<div className="rounded-md relative flex  items-center shadow-sm">
 					<input
 						id={name}
-						{...rest}
+						{...register}
+						type={type}
+						// {...rest}
+						required={required}
 						className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
 					/>
 				</div>
@@ -35,7 +46,10 @@ export default function Input({
 					</div>
 					<input
 						id={name}
-						{...rest}
+						{...register}
+						type={type}
+						// {...rest}
+						required={required}
 						className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
 					/>
 					<div className="absolute right-0 pointer-events-none pr-3 flex items-center">
@@ -50,7 +64,10 @@ export default function Input({
 					</span>
 					<input
 						id={name}
-						{...rest}
+						{...register}
+						type={type}
+						// {...rest}
+						required={required}
 						className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
 					/>
 				</div>
