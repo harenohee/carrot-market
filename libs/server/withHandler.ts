e72ddc2e9a8660f88cee45f3c7d90864fe9de577
harenohee <panda4859@gmail.com>
2023-03-23 14:48:23 +0900

    refactor: withHandler 로 함수 커스터마이징
    - 고차함수 개념
    
    by @harenohee

---
+++ libs/server/withHandler.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// 2개의 인자로 method, 실행할 함수
+export default function withHandler(
+	method: 'GET' | 'POST' | 'DELETE',
+	fn: (req: NextApiRequest, res: NextApiResponse) => void,
+) {
+	// nextJS가 실행해야 할 것을 return 해야함
+	// 그 함수를 커스터마이징하는 것 뿐
+	return async function (req: NextApiRequest, res: NextApiResponse) {
+		if (req.method !== method) {
+			return res.status(405).end()
+		}
+		try {
+			await fn(req, res)
+		} catch (error) {
+			console.log(error)
+			return res.status(500).json({ error })
+		}
+	}
+}
--- pages/api/user/enter.tsx
+++ pages/api/user/enter.tsx
@@ -1,26 +1,17 @@
+import withHandler from '@/libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
+// nextJS에서 api route를 할때는 그 function을 export default해야한다
+// 그래야 사용자가 url 호출할 때 이 function을 실행하고
+// nextJS가 req, res을 줄 수 있는 것이다
+// 함수가 return 되어야 함!!
 
 // create -> Promise를 반환 -> async await
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse,
-) {
-	if (req.method !== 'POST') {
-		res.status(401).end()
-	}
+async function handler(req: NextApiRequest, res: NextApiResponse) {
 	console.log(req.body)
-	res.status(200).end()
-
-	// await client.user.create({
-	// 	data: {
-	// 		email: 'nog',
-	// 		name: 'hi',
-	// 	},
-	// })
-	// res.json({
-	// 	ok: true,
-	// })
+	return res.status(200).end()
 }
 // pages 경로에 api 폴더를 생성함으로써 api서버가 생성됨
 // api 라우트에서 클라이언트로 정보를 보내줄 수 있다.
 // 호스팅 중인 api 라우트를 통해 데이터베이스 수정이 가능
+
+export default withHandler('POST', handler) //withHandler의 return 함수로 대치됨
