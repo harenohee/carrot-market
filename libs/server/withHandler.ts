01e412d9c5a8b49637484ac415cb509d83cc6063
harenohee <panda4859@gmail.com>
2023-04-02 20:36:56 +0900

    feat: method 형 변환
    - 배열형으로 요청하므로써 확장성 향상
    
    by @harenohee

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -4,16 +4,25 @@ export interface ResponseType {
 	ok: boolean
 	[key: string]: any
 }
+
+// GET, POST를 한번에 수행시켜주기 위해 method를 string->배열 형태로 변경
+type method = 'GET' | 'POST' | 'DELETE'
+// 2개 이상의 인자- 객체로 설정 값을 보내주는 방식으로 코드를 정리할 수 있음
+interface ConfigType {
+	methods: method[]
+	handler: (req: NextApiRequest, res: NextApiResponse) => void
+	isPrivate?: boolean
+}
 // 2개의 인자로 method, 실행할 함수
-export default function withHandler(
-	method: 'GET' | 'POST' | 'DELETE',
-	fn: (req: NextApiRequest, res: NextApiResponse) => void,
-	isPrivate: boolean,
-) {
+export default function withHandler({
+	methods,
+	isPrivate = false,
+	handler,
+}: ConfigType) {
 	// nextJS가 실행해야 할 것을 return 해야함
 	// 그 함수를 커스터마이징하는 것 뿐
 	return async function (req: NextApiRequest, res: NextApiResponse) {
-		if (req.method !== method) {
+		if (req.method && !methods.includes(req.method as any)) {
 			return res.status(405).end()
 		}
 		if (isPrivate && !req.session.user) {
@@ -23,7 +32,7 @@ export default function withHandler(
 			})
 		}
 		try {
-			await fn(req, res)
+			await handler(req, res)
 		} catch (error) {
 			console.log(error)
 			return res.status(500).json({ error })
