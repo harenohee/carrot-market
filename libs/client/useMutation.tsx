5c00010d6f79e4ef7b895104f7e2b8648ac057a5
harenohee <panda4859@gmail.com>
2023-04-02 20:30:13 +0900

    refactor: useMutation
    - useMutation 훅 리팩토링
    
    by @harenohee

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,25 +1,31 @@
 // 훅 (리액트 컴포넌트)
 
 import { useState } from 'react'
+interface UseMutationState<T> {
+	loading: boolean
+	data?: T
+	error?: object
+}
+type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>]
 
-export default function useMutation(
+export default function useMutation<T = any>(
 	url: string,
-): [
-	(data: any) => void,
-	{ loading: boolean; data: undefined | any; error: undefined | any },
-] {
+): UseMutationResult<T> {
+	const [state, setState] = useState<UseMutationState<T>>({
+		loading: false,
+		data: undefined,
+		error: undefined,
+	})
 	// function, state을 return
 	// 타입스크립트: return 타입 명시 필수
 	//return 타입: array (2개의 item; 함수,object)
 	// const [loading, setLoading] = useState(false)
 	// const [data, setData] = useState<undefined | any>(undefined)
 	// const [error, setError] = useState<undefined | any>(undefined)
-	const [state, setState] = useState({
-		loading: false,
-		data: undefined,
-		error: undefined,
-	})
+
 	function mutation(data?: any) {
+		// 화살표 함수에서 () => () 처럼 소괄호로 감싸면 return문을 작성하지 않아도 리턴된다.
+		// 반면 () => {}처럼 중괄호로 감쌌다면 return문이 있어야만 리턴 값을 반환한다.
 		setState((prev) => ({ ...prev, loading: true }))
 		fetch(url, {
 			method: 'POST',
