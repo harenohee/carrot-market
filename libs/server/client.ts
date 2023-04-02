6d185881e98ca4eb81e05313f50d50313574464c
harenohee <panda4859@gmail.com>
2023-04-02 20:34:51 +0900

    refactor: client 객체 리팩토링
    -
    
    by @harenohee

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from '@prisma/client'
+declare global {
+	var client: PrismaClient | undefined
+}
 
-export default new PrismaClient()
+// 파일을 처음 실행 시 global.client에 아무것도 들어있지 않음
+// new PrismaClient 생성
+const client = global.client || new PrismaClient()
+if (process.env.NODE_ENV === 'development') global.client = client
+
+export default client
