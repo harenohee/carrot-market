1ca8713b6d0537d7d2bf604b638f621c64b898e2
harenohee <panda4859@gmail.com>
2023-04-02 20:33:06 +0900

    feat: redirect 추가
    - data가 없거나 정상적으로 받아오지 못한 경우
    enter페이지로 redirect
    
    by @harenohee

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,12 +1,18 @@
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import useSWR from 'swr'
 export default function useUser() {
-	const fetcher = (url: string) => fetch(url).then((res) => res.json())
 	//useSWR의 첫 번째 인자:
 	// 1. fetcher가 url로 사용하기도 하고
 	// 2. fetcher함수가 캐시에서 데이터를 가져올 떄 사용하는 id로 사용됨
 	// 따라서 유저가 어떤 컴포넌트에서든지 캐시 데이터를 확인할 수 있다.
-	const { data, error } = useSWR('/api/user/me', fetcher)
+	const { data, error } = useSWR('/api/user/me')
 	const router = useRouter()
-	return data
+	useEffect(() => {
+		if (data && !data.ok) {
+			router.replace('/enter')
+		}
+		// data, router가 바뀔 때마다 실행
+	}, [data, router])
+	return { user: data?.profile, isLoading: !data && !error }
 }
