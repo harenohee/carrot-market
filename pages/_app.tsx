4200f773b3fdd6e025abe9421b6fc605bfea7b12
harenohee <panda4859@gmail.com>
2023-04-03 18:53:09 +0900

    feat: SWRConfig 모듈
    -
    
    by @harenohee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { SWRConfig } from 'swr'
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div className="w-full max-w-lg mx-auto">
-			<Component {...pageProps} />
-		</div>
+		<SWRConfig
+			value={{ fetcher: (url: string) => fetch(url).then((res) => res.json()) }}
+		>
+			<div className="w-full max-w-lg mx-auto">
+				<Component {...pageProps} />
+			</div>
+		</SWRConfig>
 	)
 }
